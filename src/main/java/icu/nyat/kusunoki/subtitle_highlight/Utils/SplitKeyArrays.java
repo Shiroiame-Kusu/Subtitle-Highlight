78388f9ddf72fd148805076316d59b14a9de4d6f
Shiroiame-Kusu <kusu@kusu.moe>
2024-06-27 20:22:56 +0800

    Ver 0.1.1-Beta.1: Update new things in 1.21 Update.

--- src/main/java/icu/nyat/kusunoki/subtitle_highlight/Utils/SplitKeyArrays.java
+++ src/main/java/icu/nyat/kusunoki/subtitle_highlight/Utils/SplitKeyArrays.java
@@ -1,20 +1,20 @@
 package icu.nyat.kusunoki.subtitle_highlight.Utils;
 
 public class SplitKeyArrays {
-    public static final String[] passiveMobs = {"allay", "axolotl", "bat", "camel", "cat", "chicken", "cod", "cow", "donkey", "fox", "frog", "glow_squid", "horse", "mooshroom", "mule", "ocelot", "parrot", "pig", "puffer_fish", "rabbit", "salmon", "sheep", "skeleton_horse", "sniffer", "snow_golem", "squid", "strider", "tadpole", "tropical_fish", "turtle", "villager", "wandering_trader", "zombie_horse"};
+    public static final String[] passiveMobs = {"allay", "armadillo", "axolotl", "bat", "camel", "cat", "chicken", "cod", "cow", "donkey", "fox", "frog", "glow_squid", "horse", "mooshroom", "mule", "ocelot", "parrot", "pig", "puffer_fish", "rabbit", "salmon", "sheep", "skeleton_horse", "sniffer", "snow_golem", "squid", "strider", "tadpole", "tropical_fish", "turtle", "villager", "wandering_trader", "zombie_horse"};
     public static final String[] neutralMobs = {"bee", "dolphin", "enderman", "goat", "iron_golem", "llama", "panda", "piglin", "polar_bear", "spider", "wolf", "zombified_piglin"};
-    public static final String[] hostileMobs = {"blaze", "creeper", "drowned", "elder_guardian", "endermite", "evoker", "ghast", "guardian", "hoglin", "husk", "illusioner", "magma_cube", "phantom", "piglin_brute", "pillager", "ravager", "shulker", "silverfish", "skeleton", "slime", "stray", "vex", "vindicator", "warden", "witch", "wither_skeleton", "zoglin", "zombie", "zombie_villager"};
+    public static final String[] hostileMobs = {"blaze", "bogged", "breeze", "creeper", "drowned", "elder_guardian", "endermite", "evoker", "ghast", "guardian", "hoglin", "husk", "illusioner", "magma_cube", "phantom", "piglin_brute", "pillager", "ravager", "shulker", "silverfish", "skeleton", "slime", "stray", "vex", "vindicator", "warden", "witch", "wither_skeleton", "zoglin", "zombie", "zombie_villager"};
     public static final String[] boss = {"ender_dragon", "wither"};
     public static final String[] vehicle = {"boat", "minecart"};
-    public static final String[] Catapult = {"arrow", "egg", "ender_eye", "ender_pearl", "potion", "shulker_bullet", "snowball"};
+    public static final String[] Catapult = {"arrow", "wind_charge", "breeze_wind_charge", "egg", "ender_eye", "ender_pearl", "potion", "shulker_bullet", "snowball"};
     public static final String[] explosive = {"firework_rocket", "lightning_bolt", "tnt"};
-    public static final String[] decoration = {"armor_stand", "glow_item_frame", "item_frame", "painting"};
+    public static final String[] decoration = {"armor_st    and", "glow_item_frame", "item_frame", "painting"};
     public static final String[] hurt = {"big_fall", "burn", "death", "explode", "extinguish_fire", "hurt", "small_fall", "freeze_hurt", "hurt_drown", "hurt_on_fire"};
     public static final String[] interact = {"anvil", "barrel", "bell", "big_dripleaf", "button", "cake", "chest", "comparator", "composter", "door", "enchantment_table", "end_portal_frame", "fence_gate", "grindstone", "growing_plant", "honey_block", "lever", "note_block", "pressure_plate", "pumpkin", "respawn_anchor", "sculk_sensor", "shulker_box", "sign", "smithing_table", "sweet_berry_bush", "trapdoor", "tripwire"};
     public static final String[] working = {"amethyst_block", "beacon", "beehive", "blastfurnace", "brewing_stand", "bubble_column", "candle", "conduit", "decorated_pot", "dispenser", "end_portal", "furnace", "iron_trapdoor", "piston", "pointed_dripstone", "portal", "redstone_torch", "sculk", "sculk_catalyst", "smoker", "sniffer_egg", "water"};
     public static final String[] dangerousBlocks = {"campfire", "fire", "lava", "sculk_shrieker"};
     public static final String[] crops = {"chorus_flower", "frogspawn"};
-    public static final String[] weapon = {"crossbow", "trident"};
-    public static final String[] armor = {"armor", "shield"};
+    public static final String[] weapon = {"crossbow", "trident", "mace"};
+    public static final String[] armor = {"armor", "shield", "wolf_armor"};
     public static final String[] tool = {"axe", "bottle", "brush", "bucket", "bundle", "flintandsteel", "hoe", "goat_horn", "lodestone_compass", "shears", "shovel", "spyglass", "totem"};
 }
